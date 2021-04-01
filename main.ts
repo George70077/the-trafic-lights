f33594812f8c0e0d0340719154c07c11c14bf918
George70077 <79929846+George70077@users.noreply.github.com>
2021-04-01 13:22:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,47 @@
-// pin 0 is red
-// pin 1 is green
-// it turns on lights when A is pressed.
+// pin 0 is green pin 1 is yellow
+// pin 2 is redwhen A is pressed.
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
+// pin 0 is green pin 1 is yellow
+// pin 2 is redwhen A is pressed.
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P0, 1)
 })
-// turns out 
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    while (true) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(3000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(4000)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
 })
+input.onGesture(Gesture.Shake, function () {
+	
+})
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
 basic.forever(function () {
 	
 })
